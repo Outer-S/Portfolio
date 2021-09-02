8967a3fb55b863ce6d12bfa05ca8d71286cdefd4
Brahim-Amrouche <amroucheb4@gmail.com>
2021-09-02 15:51:03 +0100

    first commit

---
+++ components/SideNav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SideNav() {
+    return (
+        <div className="SideNav">
+            <ul>
+                <li><a href="#none">About Us</a></li>
+                <li><a href="#none">Projects</a></li>
+                <li><a href="#none">Technologies</a></li>
+                <li><a href="#none">Contact Us</a></li>
+            </ul>           
+        </div>
+    )
+}
+
+export default SideNav
